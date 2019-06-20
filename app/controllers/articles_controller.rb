9afa8bcd5159f018a5b80ca985882e358497d8b6
Joshua MacSween <joshmacsween@gmail.com>
2019-06-20 06:00:22 -0600

    something works

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,24 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article has been succesfully created"
+      @article.save
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
