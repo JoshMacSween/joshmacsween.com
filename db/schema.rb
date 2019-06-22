7d3a8608f9991bbde0ee2a2f12b6fb4659eaa2c0
Joshua MacSween <joshmacsween@gmail.com>
2019-06-22 16:11:46 -0600

    going back

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_112116) do
+ActiveRecord::Schema.define(version: 2019_06_21_175856) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "quotes", force: :cascade do |t|
+    t.text "content"
+    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
