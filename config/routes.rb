0b6dde86d361087816948c4b9bffd3070b9ad432
Joshua MacSween <joshmacsween@gmail.com>
2019-07-16 15:07:12 -0600

    tore out quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get '/home', to: 'pages#home'
-  get '/about', to: 'pages#about'
-  get '/blog', to: 'pages#blog'
-  get '/contact', to: 'pages#contact'
-
-  resources :articles
-  resources :quotes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,10 +20,8 @@ ActiveRecord::Schema.define(version: 2019_06_21_175856) do
   end
 
   create_table "quotes", force: :cascade do |t|
-    t.text "content"
-    t.string "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", null: true
+    t.datetime "updated_at", null: true
   end
 
 end
